4fd6f866b434037a0b2d1023522914d2e38f3906
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2024-03-30 11:45:45 +0600

    backend.feat: added Photo model

---
+++ backend/models/Photo.ts
@@ -0,0 +1,30 @@
+import mongoose, { Types } from 'mongoose';
+import User from './User';
+
+const Schema = mongoose.Schema;
+
+const PhotoSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => {
+        const user = await User.findById(value);
+        return Boolean(user);
+      },
+      message: 'VALIDATOR ERROR: User does not exist!',
+    },
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+const Photo = mongoose.model('Cocktail', PhotoSchema);
+export default Photo;
